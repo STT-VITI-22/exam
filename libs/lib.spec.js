5dc7173456b98300cda7df4a9f967c9a590115af
AnastasiiaIvanina <131382318+AnastasiiaIvanina@users.noreply.github.com>
2023-04-21 09:54:49 +0300

    good exam

---
+++ libs/lib.spec.js
@@ -0,0 +1,25 @@
+import {toExponential} from "./variant6";
+
+describe('Test suite for testing variant6.js', () => {
+  describe('Test suite for testing toExponential function', () => {
+    it('should return string ', () => {
+
+     // assert.isTrue(isNaN(toExponential('e')));
+      assert.equal(toExponential(4),4.0 )
+    });
+
+
+  });
+  describe('Test suite for testing roundDigits function', () => {
+    it('should return roundDigits ', () => {
+
+    });
+
+
+  });
+
+
+
+
+});
+
