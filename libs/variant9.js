93c390c28163b6e43152b2a770674c7b3bc7c649
niksond345 <lallaypich@gmail.com>
2023-12-06 10:16:23 +0200

    Varint_9 done

--- libs/variant9.js
+++ libs/variant9.js
@@ -1,81 +1,80 @@
-import {flatten} from "./variant10";
+import {flatten} from "./variant9";
 
-/**
- * Re-shape a multi dimensional array to fit the specified dimensions
- * @param {Array} array           Array to be reshaped
- * @param {Array.<number>} sizes  List of sizes for each dimension
- * @returns {Array}               Array whose data has been formatted to fit the
- *                                specified dimensions
- *
- * @throws {DimensionError}       If the product of the new dimension sizes does
- *                                not equal that of the old ones
- */
-export function reshape (array, sizes) {
-  const flatArray = flatten(array)
-  const currentLength = flatArray.length
+import { reshape } from "./yourModuleName"; // Replace with your actual module name
 
-  if (!Array.isArray(array) || !Array.isArray(sizes)) {
-    throw new TypeError('Array expected')
-  }
+// Mock or import any additional dependencies needed for testing
 
-  if (sizes.length === 0) {
-    throw new DimensionError(0, currentLength, '!=')
-  }
+describe('reshape', () => {
+  test('reshape should reshape the array correctly', () => {
+    const inputArray = [[1, 2], [3, 4]];
+    const inputSizes = [4];
+    const expectedOutput = [[1, 2, 3, 4]];
 
-  sizes = processSizesWildcard(sizes, currentLength)
-  const newLength = product(sizes)
-  if (currentLength !== newLength) {
-    throw new DimensionError(
-      newLength,
-      currentLength,
-      '!='
-    )
-  }
+    // Assuming that flatten, processSizesWildcard, product, and _reshape are working correctly
+    // You may want to use mocks or stubs for these functions if needed
 
-  try {
-    return _reshape(flatArray, sizes)
-  } catch (e) {
-    if (e instanceof DimensionError) {
-      throw new DimensionError(
-        newLength,
-        currentLength,
-        '!='
-      )
-    }
-    throw e
-  }
-}
+    // Mocking flatten
+    jest.mock('./yourModuleName', () => ({
+      flatten: jest.fn(array => array.flat()),
+    }));
 
-/**
- * Replaces the wildcard -1 in the sizes array.
- * @param {Array.<number>} sizes  List of sizes for each dimension. At most on wildcard.
- * @param {number} currentLength  Number of elements in the array.
- * @throws {Error}                If more than one wildcard or unable to replace it.
- * @returns {Array.<number>}      The sizes array with wildcard replaced.
- */
-export function processSizesWildcard (sizes, currentLength) {
-  const newLength = product(sizes)
-  const processedSizes = sizes.slice()
-  const WILDCARD = -1
-  const wildCardIndex = sizes.indexOf(WILDCARD)
+    // Mocking other functions
+    jest.mock('./yourModuleName', () => ({
+      flatten: jest.fn(array => array.flat()),
+      processSizesWildcard: jest.fn(sizes => sizes),
+      product: jest.fn(sizes => sizes.reduce((acc, val) => acc * val, 1)),
+      _reshape: jest.fn((array, sizes) => array), // Mocking _reshape to return the input array
+    }));
 
-  const isMoreThanOneWildcard = sizes.indexOf(WILDCARD, wildCardIndex + 1) >= 0
-  if (isMoreThanOneWildcard) {
-    throw new Error('More than one wildcard in sizes')
-  }
+    const result = reshape(inputArray, inputSizes);
 
-  const hasWildcard = wildCardIndex >= 0
-  const canReplaceWildcard = currentLength % newLength === 0
+    expect(result).toEqual(expectedOutput);
+  });
 
-  if (hasWildcard) {
-    if (canReplaceWildcard) {
-      processedSizes[wildCardIndex] = -currentLength / newLength
-    } else {
-      throw new Error('Could not replace wildcard, since ' + currentLength + ' is no multiple of ' + (-newLength))
-    }
-  }
-  return processedSizes
-}
+  // Add more test cases for different scenarios
+});
+
+import { processSizesWildcard } from "./yourModuleName"; // Replace with your actual module name
+
+describe('processSizesWildcard', () => {
+  test('should replace a single wildcard correctly', () => {
+    const sizes = [2, -1, 3];
+    const currentLength = 6;
+    const expectedOutput = [2, -3, 3];
+
+    const result = processSizesWildcard(sizes, currentLength);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should handle no wildcard in sizes', () => {
+    const sizes = [2, 3, 4];
+    const currentLength = 24;
+    const expectedOutput = sizes.slice(); // Expecting the sizes array to remain unchanged
+
+    const result = processSizesWildcard(sizes, currentLength);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should throw an error for more than one wildcard', () => {
+    const sizes = [-1, 3, -1];
+    const currentLength = 12;
+
+    expect(() => {
+      processSizesWildcard(sizes, currentLength);
+    }).toThrowError('More than one wildcard in sizes');
+  });
+
+  test('should throw an error if unable to replace the wildcard', () => {
+    const sizes = [2, -1, 3];
+    const currentLength = 7; // Unable to replace the wildcard with the given current length
+
+    expect(() => {
+      processSizesWildcard(sizes, currentLength);
+    }).toThrowError('Could not replace wildcard');
+  });
+});
 
 /**
  * Computes the product of all array elements.
@@ -86,118 +85,167 @@ function product (array) {
   return array.reduce((prev, curr) => prev * curr, 1)
 }
 
-/**
- * Iteratively re-shape a multi dimensional array to fit the specified dimensions
- * @param {Array} array           Array to be reshaped
- * @param {Array.<number>} sizes  List of sizes for each dimension
- * @returns {Array}               Array whose data has been formatted to fit the
- *                                specified dimensions
- */
+import { _reshape } from "./yourModuleName"; // Replace with your actual module name
 
-function _reshape (array, sizes) {
-  // testing if there are enough elements for the requested shape
-  let tmpArray = array
-  let tmpArray2
-  // for each dimensions starting by the last one and ignoring the first one
-  for (let sizeIndex = sizes.length - 1; sizeIndex > 0; sizeIndex--) {
-    const size = sizes[sizeIndex]
-    tmpArray2 = []
+describe('_reshape', () => {
+  test('should reshape array correctly', () => {
+    const inputArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const inputSizes = [3, 3];
+    const expectedOutput = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-    // aggregate the elements of the current tmpArray in elements of the requested size
-    const length = tmpArray.length / size
-    for (let i = 0; i < length; i++) {
-      tmpArray2.push(tmpArray.slice(i * size, (i + 1) * size))
-    }
-    // set it as the new tmpArray for the next loop turn or for return
-    tmpArray = tmpArray2
-  }
+    const result = _reshape(inputArray, inputSizes);
 
-  return tmpArray
-}
+    expect(result).toEqual(expectedOutput);
+  });
 
-/**
- * Squeeze a multi dimensional array
- * @param {Array} array
- * @param {Array} [size]
- * @returns {Array} returns the array itself
- */
-export function squeeze (array, size) {
-  const s = size || arraySize(array)
+  test('should handle reshaping with different sizes', () => {
+    const inputArray = [1, 2, 3, 4, 5, 6];
+    const inputSizes = [2, 3];
+    const expectedOutput = [[1, 2, 3], [4, 5, 6]];
 
-  // squeeze outer dimensions
-  while (Array.isArray(array) && array.length === 1) {
-    array = array[0]
-    s.shift()
-  }
+    const result = _reshape(inputArray, inputSizes);
 
-  // find the first dimension to be squeezed
-  let dims = s.length
-  while (s[dims - 1] === 1) {
-    dims--
-  }
+    expect(result).toEqual(expectedOutput);
+  });
 
-  // squeeze inner dimensions
-  if (dims < s.length) {
-    array = _squeeze(array, dims, 0)
-    s.length = dims
-  }
+  test('should handle reshaping with uneven sizes', () => {
+    const inputArray = [1, 2, 3, 4, 5, 6, 7, 8];
+    const inputSizes = [2, 4];
+    const expectedOutput = [[1, 2, 3, 4], [5, 6, 7, 8]];
 
-  return array
-}
+    const result = _reshape(inputArray, inputSizes);
 
-/**
- * Recursively squeeze a multi dimensional array
- * @param {Array} array
- * @param {number} dims Required number of dimensions
- * @param {number} dim  Current dimension
- * @returns {Array | *} Returns the squeezed array
- * @private
- */
-function _squeeze (array, dims, dim) {
-  let i, ii
+    expect(result).toEqual(expectedOutput);
+  });
 
-  if (dim < dims) {
-    const next = dim + 1
-    for (i = 0, ii = array.length; i < ii; i++) {
-      array[i] = _squeeze(array[i], dims, next)
-    }
-  } else {
-    while (Array.isArray(array)) {
-      array = array[0]
-    }
-  }
+  // Add more test cases for different scenarios
+});
+import { squeeze } from "./yourModuleName"; // Replace with your actual module name
 
-  return array
-}
+describe('squeeze', () => {
+  test('should squeeze outer dimensions', () => {
+    const inputArray = [[[1, 2]]];
+    const expectedOutput = [[1, 2]];
 
-/**
- * Unsqueeze a multi dimensional array: add dimensions when missing
- *
- * Paramter `size` will be mutated to match the new, unqueezed matrix size.
- *
- * @param {Array} array
- * @param {number} dims       Desired number of dimensions of the array
- * @param {number} [outer]    Number of outer dimensions to be added
- * @param {Array} [size] Current size of array.
- * @returns {Array} returns the array itself
- * @private
- */
-export function unsqueeze (array, dims, outer, size) {
-  const s = size || arraySize(array)
+    const result = squeeze(inputArray);
 
-  // unsqueeze outer dimensions
-  if (outer) {
-    for (let i = 0; i < outer; i++) {
-      array = [array]
-      s.unshift(1)
-    }
-  }
+    expect(result).toEqual(expectedOutput);
+  });
 
-  // unsqueeze inner dimensions
-  array = _unsqueeze(array, dims, 0)
-  while (s.length < dims) {
-    s.push(1)
-  }
+  test('should squeeze inner dimensions', () => {
+    const inputArray = [[[1], [2], [3]]];
+    const expectedOutput = [1, 2, 3];
 
-  return array
-}
+    const result = squeeze(inputArray);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should handle array with no dimensions to squeeze', () => {
+    const inputArray = [1, 2, 3];
+    const expectedOutput = inputArray.slice();
+
+    const result = squeeze(inputArray);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should handle specified size array', () => {
+    const inputArray = [[[1], [2], [3]]];
+    const inputSize = [1, 3];
+    const expectedOutput = [[1, 2, 3]];
+
+    const result = squeeze(inputArray, inputSize);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  // Add more test cases for different scenarios
+});
+
+import { _squeeze } from "./yourModuleName"; // Replace with your actual module name
+
+describe('_squeeze', () => {
+  test('should recursively squeeze array to the specified dimensions', () => {
+    const inputArray = [[[1], [2], [3]]];
+    const expectedOutput = [1, 2, 3];
+
+    const result = _squeeze(inputArray, 1, 0);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should handle array with no dimensions to squeeze', () => {
+    const inputArray = [1, 2, 3];
+    const expectedOutput = inputArray.slice();
+
+    const result = _squeeze(inputArray, 1, 0);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  test('should handle squeezing inner dimensions with different sizes', () => {
+    const inputArray = [[[[1], [2]], [[3], [4]]]];
+    const expectedOutput = [[1, 2], [3, 4]];
+
+    const result = _squeeze(inputArray, 2, 0);
+
+    expect(result).toEqual(expectedOutput);
+  });
+
+  // Add more test cases for different scenarios
+});
+
+import { unsqueeze } from "./yourModuleName"; // Replace with your actual module name
+
+describe('unsqueeze', () => {
+  test('should unsqueeze outer dimensions', () => {
+    const inputArray = [[1, 2, 3]];
+    const inputSize = [3];
+    const expectedOutput = [[[1, 2, 3]]];
+    const expectedSize = [1, 3];
+
+    const result = unsqueeze(inputArray, 2, 1, inputSize);
+
+    expect(result).toEqual(expectedOutput);
+    expect(inputSize).toEqual(expectedSize);
+  });
+
+  test('should unsqueeze inner dimensions', () => {
+    const inputArray = [1, 2, 3];
+    const inputSize = [3];
+    const expectedOutput = [[[1], [2], [3]]];
+    const expectedSize = [3, 1];
+
+    const result = unsqueeze(inputArray, 2, 0, inputSize);
+
+    expect(result).toEqual(expectedOutput);
+    expect(inputSize).toEqual(expectedSize);
+  });
+
+  test('should handle array with no dimensions to unsqueeze', () => {
+    const inputArray = 1;
+    const inputSize = [1];
+    const expectedOutput = inputArray;
+    const expectedSize = inputSize.slice();
+
+    const result = unsqueeze(inputArray, 1, 1, inputSize);
+
+    expect(result).toEqual(expectedOutput);
+    expect(inputSize).toEqual(expectedSize);
+  });
+
+  test('should handle unsqueezing with different sizes', () => {
+    const inputArray = [[1, 2], [3, 4]];
+    const inputSize = [2, 2];
+    const expectedOutput = [[[1, 2]], [[3, 4]]];
+    const expectedSize = [2, 1, 2];
+
+    const result = unsqueeze(inputArray, 3, 1, inputSize);
+
+    expect(result).toEqual(expectedOutput);
+    expect(inputSize).toEqual(expectedSize);
+  });
+
+  // Add more test cases for different scenarios
+});
